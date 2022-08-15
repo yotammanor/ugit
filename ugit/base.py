c0ce9d1b017b41a81c6766ba61bf86fbd0cab30e
Yotam <yotammanor@gmail.com>
2022-08-15 12:53:35 +0300

    ugit step 11 (in write-tree, hash directories as trees)

--- ugit/base.py
+++ ugit/base.py
@@ -4,17 +4,24 @@ from . import data
 
 
 def write_tree(directory='.'):
+    entries = []
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
             if is_ignored(full):
                 continue
             if entry.is_file(follow_symlinks=False):
+                type_ = 'blob'
                 with open(full, 'rb') as f:
-                    print(data.hash_object(f.read()), full)
+                    oid = data.hash_object(f.read())
             elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
-    # todo: actually create the tree object
+                type_ = 'tree'
+                oid = write_tree(full)
+            entries.append((entry.name, oid, type_))
+    tree = ''.join(f'{type_} {oid} {name}\n'
+                   for name, oid, type_
+                   in sorted(entries))
+    return data.hash_object(tree.encode(), 'tree')
 
 
 def is_ignored(path):
