399cf1461a8dd4c0d3cd4945df9930b329f31196
Yotam <yotammanor@gmail.com>
2022-08-15 11:12:09 +0300

    ugit step 4 (add git hash-object)

--- ugit/data.py
+++ ugit/data.py
@@ -1,6 +1,14 @@
 import os
+import hashlib
 
 GIT_DIR = '.ugit'
 
 def init():
-    os.makedirs(GIT_DIR)
+    os.makedirs(GIT_DIR, exist_ok=True)
+    os.makedirs(f'{GIT_DIR}/objects', exist_ok=True)
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
