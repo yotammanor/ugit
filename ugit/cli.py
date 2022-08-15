827a5a770871d747c22ceb6557aa3382b679bff4
Yotam <yotammanor@gmail.com>
2022-08-15 11:16:03 +0300

    ugit step 5 (add git cat-file)

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from . import data
 
@@ -20,6 +21,10 @@ def parse_args():
     hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument('file')
 
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
+
     return parser.parse_args()
 
 def init(args):
@@ -29,3 +34,7 @@ def init(args):
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
