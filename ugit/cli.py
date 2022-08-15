e55bc8b3c2b9a4b875dc9eca6d6d58c72864ab1d
Yotam <yotammanor@gmail.com>
2022-08-15 11:05:39 +0300

    ugit step 3 (add git init)

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+from . import data
 
 def main():
     args = parse_args()
@@ -16,4 +19,5 @@ def parse_args():
     return parser.parse_args()
 
 def init(args):
-    print('Hello, World!')
+    data.init()
+    print(f'Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
---
+++ ugit/data.py
@@ -0,0 +1,6 @@
+import os
+
+GIT_DIR = '.ugit'
+
+def init():
+    os.makedirs(GIT_DIR)
