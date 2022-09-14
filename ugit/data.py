22d536df4a72030163d17ce5f57cdfc7b80d7eb2
Yotam <yotammanor@gmail.com>
2022-09-14 12:47:30 +0300

    ugit step 50 (add data.delete_ref function.)

--- ugit/data.py
+++ ugit/data.py
@@ -48,6 +48,11 @@ def get_ref(ref, deref=True) -> RefValue:
     return _get_ref_internal(ref, deref)[1]
 
 
+def delete_ref(ref, deref=True):
+    ref = _get_ref_internal(ref, deref)[0]
+    os.remove(f'{GIT_DIR}/{ref}')
+
+
 def _get_ref_internal(ref: str, deref: bool) -> tuple[str, RefValue]:
     ref_path = f'{GIT_DIR}/{ref}'
     value = None
