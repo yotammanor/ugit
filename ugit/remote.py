53b4e44a2f6d42f486dd503ac6fff0c0ac6679bb
Yotam <yotammanor@gmail.com>
2022-09-17 15:42:59 +0300

    ugit step 59 (update fetch to create the missing refs in refs/remote/)

--- ugit/remote.py
+++ ugit/remote.py
@@ -1,10 +1,17 @@
+import os
+
 from . import data
+
+REMOTE_REFS_BASE = 'refs/heads/'
+LOCAL_REFS_BASE = 'refs/remote'
 
 
 def fetch(remote_path):
-    print('Will fetch the following refs:')
-    for refname, _ in _get_remote_refs(remote_path, 'refs/heads').items():
-        print(f' -{refname}')
+    refs = _get_remote_refs(remote_path, REMOTE_REFS_BASE)
+    for remote_name, value in refs.items():
+        refname = os.path.relpath(remote_name, REMOTE_REFS_BASE)
+        data.update_ref(f'{LOCAL_REFS_BASE}/{refname}',
+                        data.RefValue(symbolic=False, value=value))
 
 
 def _get_remote_refs(remote_path, prefix=''):
